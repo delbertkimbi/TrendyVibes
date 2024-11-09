7ed75f9469004b9d6912eedd0407a020c8563b54
delbertkimbi <delbertdrums5@gmail.com>
2024-11-09 23:31:10 +0100

    Okay

---
+++ trendy_vibes/android/app/src/main/kotlin/com/example/trendy_vibes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trendy_vibes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ trendy_vibes/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'TrendyVibes',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1DB954), // Spotify-like green
+        ),
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1DB954),
+          brightness: Brightness.dark,
+        ),
+        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme),
+      ),
+      themeMode: ThemeMode.system, 
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
